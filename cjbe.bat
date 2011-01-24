79260e050b5043c67230cdd13d14a17467909a6c
Contra <contra@maricopa.edu>
2011-01-24 16:19:17 -0700

    file cleaning. fixed batch

--- cjbe.bat
+++ cjbe.bat
@@ -1,5 +1,8 @@
 @echo off
-echo "Original version by Ando Saabas http://cs.ioc.net/~ando/jbe/"
-echo "Code Maintained by Contra"
-cd bin
+echo Original version by Ando Saabas http://cs.ioc.net/~ando/jbe/
+echo Code Maintained by Contra
+cd out
+cd production
+cd cjbe
 java net.rec.contra.cjbe.editor.BrowserApplication
+pause
