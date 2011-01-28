cf6951b0f79690925e80f27f75271118bfdc0428
Contra <contra@maricopa.edu>
2011-01-28 10:59:10 -0700

    test control flow diagram. no graphics yet, still figuring this out

--- src/net/rec/contra/cjbe/editor/detail/attributes/code/DiagramDisplay.java
+++ src/net/rec/contra/cjbe/editor/detail/attributes/code/DiagramDisplay.java
@@ -1,17 +1,18 @@
 package net.rec.contra.cjbe.editor.detail.attributes.code;
 
 import net.rec.contra.cjbe.editor.BrowserInternalFrame;
+import net.rec.contra.cjbe.editor.detail.attributes.code.graph.cfg.Block;
+import net.rec.contra.cjbe.editor.detail.attributes.code.graph.cfg.BlockGraph;
 import net.rec.contra.cjbe.editor.detail.attributes.code.graph.cfg.InstructionGraph;
 import org.apache.bcel.classfile.ClassParser;
 import org.apache.bcel.classfile.JavaClass;
 import org.apache.bcel.generic.ClassGen;
-import org.apache.bcel.generic.InstructionList;
+import org.apache.bcel.generic.InstructionHandle;
 import org.apache.bcel.generic.MethodGen;
-import org.apache.bcel.verifier.structurals.ControlFlowGraph;
-import org.gjt.jclasslib.structures.ClassFile;
 
 import javax.swing.*;
 import java.io.IOException;
+import java.util.Iterator;
 
 
 public class DiagramDisplay extends JEditorPane {
@@ -33,7 +34,37 @@ public class DiagramDisplay extends JEditorPane {
         ClassGen cg = new ClassGen(javaClass);
         MethodGen mg = new MethodGen(cg.getMethods()[methodIndex], cg.getClassName(), cg.getConstantPool());
         InstructionGraph ig = new InstructionGraph(mg, true);
-        setText(ig.toString());
+        BlockGraph blockGraph = new BlockGraph(ig);
+        String pageText = "";
+        Iterator<Block> it = blockGraph.iterator();
+        while(it.hasNext()){
+            Block bk = it.next();
+            pageText += "Block " + bk.getIndex() + ":\n";
+            if(bk.getSuccs() != null){
+                pageText += " Hops to";
+                for (Block bz : bk.getSuccs()){
+                    pageText += "\n - Block " + bz.getIndex();
+                }
+                pageText += "\n";
+            }
+            InstructionHandle[] handles = bk.getMethod().getInstructionList().getInstructionHandles();
+            for(int i = bk.getHead().getPosition(); i <= bk.getTail().getPosition(); i++){
+                if(i < handles.length){
+                    pageText += handles[i].toString() + "\n";
+                } else {
+                    pageText += " INVALID\n";
+                }
+                /*
+                if(bk.getTail().getPosition() > mg.getInstructionList().getInstructionHandles().length){
+                    System.out.println(mg.getName() + " had invalid unit data.");
+                    pageText += "INVALID UNIT DATA\n";
+                    break;
+                } else {
+                    pageText += handles[i].toString() + "\n";
+                } */
+            }
+        }
+        setText(pageText);
     }
 
 }
